945731a96d3c943f1c16056e7ab5fec2d5fa04c3
luizrebustine <luizrebustine.b@gmail.com>
2023-03-29 14:21:19 -0300

    Initial commit

---
+++ aula011/ex008.js
@@ -0,0 +1,7 @@
+var vel = 90
+console.log(`A velocidade do seu carro é ${vel} Km/h`)
+if (vel >60){
+    console.log('Sua velocidade está muito alta! Você foi multado.')
+}
+console.log('Dirija sempre usando cinto de segurança')
+  
