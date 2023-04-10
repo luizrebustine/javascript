c7e88521884af1a1ddf7bb71362493c359211fb4
luizrebustine <luizrebustine.b@gmail.com>
2023-04-10 16:50:08 -0300

    Exercício Tabuada v1.0
    Tabuada Funcional com JavaScript, HTML e CSS

--- exercicio02/ambiente.js
+++ exercicio02/ambiente.js
@@ -1,6 +1,19 @@
-var i = 5
+let t1 = 0
+let t2 = 5
+while(t1<10){
+    t1++
+    var tr = t1*t2
+    console.log(`${t1}x${t2}=${tr}`)
+}
+
+
+
+
+
+
+/*var i = 5
 var f = 12
 var p = 2
 for(i; i<=f;i+=p){
     console.log('passario')
-}
+}*/
